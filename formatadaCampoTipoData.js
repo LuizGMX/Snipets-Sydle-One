f8aa7007150c1debbc4e6be415fa03d7e33f36ce
Luiz Gustavo <44658199+LuizGMX@users.noreply.github.com>
2022-03-18 09:22:17 -0300

    Update formatadaCampoTipoData.js

--- formatadaCampoTipoData.js
+++ formatadaCampoTipoData.js
@@ -3,10 +3,14 @@
 
 function formataData(data) {
     let dataFormatoIso = data.toISOString();
+    
     let dataSemFormatacaoIso = dataFormatoIso.substring(0, 10);
+    
     let ano = dataSemFormatacaoIso.substring(0, 4);
     let mes = dataSemFormatacaoIso.substring(5, 7);
     let dia = dataSemFormatacaoIso.substring(8, 10);
+    
     let horario = dataFormatoIso.substring(11, 16);
+    
     return dia + "/" + mes + "/" + ano + " às " + horario;
 }
